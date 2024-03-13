77bc7a74508418ed8b648aff5389a935ee9e8a67
Maikel <github@maikel.dev>
2024-03-13 10:50:23 +0100

    Designed basic login pageº
    Designed basic login page

--- lib/fedigrindr/application.ex
+++ lib/fedigrindr/application.ex
@@ -11,8 +11,7 @@ defmodule Fedigrindr.Application do
       FedigrindrWeb.Telemetry,
       Fedigrindr.Repo,
       {Ecto.Migrator,
-        repos: Application.fetch_env!(:fedigrindr, :ecto_repos),
-        skip: skip_migrations?()},
+       repos: Application.fetch_env!(:fedigrindr, :ecto_repos), skip: skip_migrations?()},
       {DNSCluster, query: Application.get_env(:fedigrindr, :dns_cluster_query) || :ignore},
       {Phoenix.PubSub, name: Fedigrindr.PubSub},
       # Start the Finch HTTP client for sending emails
